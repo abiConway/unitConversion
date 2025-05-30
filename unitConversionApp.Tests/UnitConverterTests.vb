3345918e159eb76a89b8d679404f9d4dd9fee686
abiConway <aconway@uccs.edu>
2025-05-30 09:05:04 -0600

    test

---
+++ unitConversionApp.Tests/UnitConverterTests.vb
@@ -0,0 +1,38 @@
+﻿Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports unitConversionApp
+
+<TestClass>
+Public Class UnitConverterTests
+
+    <TestMethod>
+    Public Sub InchesToCentimeters_ReturnsCorrectValue()
+        ' Arrange
+        Dim input As Double = 10
+        Dim expected As Double = 25.4
+
+        ' Act
+        Dim actual As Double = UnitConverter.Convert(input, "Inches to Centimeters")
+
+        ' Assert
+        Assert.AreEqual(expected, actual, 0.001)
+    End Sub
+
+    <TestMethod>
+    Public Sub PoundsToKilograms_ReturnsCorrectValue()
+        Dim input As Double = 100
+        Dim expected As Double = 45.3592
+
+        Dim actual As Double = UnitConverter.Convert(input, "Pounds to Kilograms")
+
+        Assert.AreEqual(expected, actual, 0.001)
+    End Sub
+
+    <TestMethod>
+    <ExpectedException(GetType(ArgumentException))>
+    Public Sub InvalidConversion_ThrowsException()
+        Dim input As Double = 5
+
+        UnitConverter.Convert(input, "Invalid Type")
+    End Sub
+
+End Class
--- unitConversionApp/Form1.vb
+++ unitConversionApp/Form1.vb
@@ -55,3 +55,4 @@
 
 
 End Class
+
